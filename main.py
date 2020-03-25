311034e7436e2d3e17919e48838249729820e6af
pan Alexey <mr.alex.pan@list.ru>
2020-03-25 22:21:57 +0300

    make it better

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # app.py
 from flask import Flask, request, jsonify, render_template
-from data import db_session
-from data.animals import Animals
+# from data import db_session
+# from data.animals import Animals
 # from waitress import serve
 # import os
 
