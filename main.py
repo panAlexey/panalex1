857ce9c3c9ecaf4bda03743155e04f0affefa5fb
pan Alexey <mr.alex.pan@list.ru>
2020-03-25 13:03:12 +0300

    проверяю как работают изменения через гит

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def post_something():
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return render_template('index.html', title='Вебсервис', username='Alex')
+    return render_template('index.html', title='Вебсервис', username='Alexis')
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
